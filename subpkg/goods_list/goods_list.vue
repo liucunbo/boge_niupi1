1e38de178036e9fb7d51d02481fa802ca241e773
liucunbo <1241278121@qq.com>
2021-09-08 18:32:04 +0800

    完成了首页功能的开发

---
+++ subpkg/goods_list/goods_list.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    Good_list
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
